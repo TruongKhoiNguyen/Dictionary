058ef902e1ad1cb776ad82d43bd1c2208ac4720e
TruongKhoiNguyen <78544282+TruongKhoiNguyen@users.noreply.github.com>
2021-09-26 10:29:27 +0700

    1.0.10: Improve main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,30 @@
 package main.java;
 
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Dictionary dictionary = new Dictionary();
         DictionaryManagement dictMan = new DictionaryManagement(dictionary);
         DictionaryCommandline dictCom = new DictionaryCommandline(dictionary);
 
-        dictMan.insertFromCommandline();
-        dictCom.showAllWords();
+        loop: while (true) {
+            System.out.println("Options: ");
+            System.out.println("1. Insert");
+            System.out.println("2. Show");
+            System.out.println("3. Exit");
+
+            int n = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (n) {
+                case 1 -> dictMan.insertFromCommandline();
+                case 2 -> dictCom.showAllWords();
+                case 3 -> { break loop; }
+                default -> {}
+            }
+        }
     }
 }
