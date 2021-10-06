f7211dbeffc6d78ee71408a6fce291a50261f014
TruongKhoiNguyen <78544282+TruongKhoiNguyen@users.noreply.github.com>
2021-10-06 19:01:47 +0700

    1.5.1: Minor changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import main.java.DictionaryCommandLineApp;
 public class Main {
     public static void main(String[] args) {
         DictionaryCommandLineApp app = new DictionaryCommandLineApp();
-        DictionaryManager manager = new DictionaryManager();
 
         while (app.isRunning()) {
             app.dictionaryBasic();
