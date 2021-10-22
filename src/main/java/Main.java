7d84e1389e96bec14f69a282e752350060b701fa
Hoàng Quý <78021023+quysterben@users.noreply.github.com>
2021-10-23 00:51:01 +0700

    Revert "1.8.8 design EditGUI"
    This reverts commit 9e734dedd7885cfc1a5b8033c780ceee7ce88562.

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+    public static void main(String[] args) {
+        DictionaryCommandLineApp app = new DictionaryCommandLineApp();
+
+        while (app.isRunning()) {
+            app.dictionaryBasic();
+        }
+    }
+}
