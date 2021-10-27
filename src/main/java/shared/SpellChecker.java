d03d98637071485fd768bc2ae19c5a63e02ea564
TruongKhoiNguyen <78544282+TruongKhoiNguyen@users.noreply.github.com>
2021-10-27 21:15:57 +0700

    1.10.11: Minor changes

--- src/main/java/shared/SpellChecker.java
+++ src/main/java/shared/SpellChecker.java
@@ -1,13 +1,13 @@
 package shared;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.lang.Math.*;
+import static java.lang.Math.ceil;
+import static java.lang.Math.min;
 
 public record SpellChecker(DictionaryManager dictionaryManager) {
     private static final int EDIT_DISTANCE_THRESHOLD = 3;
@@ -41,66 +41,51 @@ public record SpellChecker(DictionaryManager dictionaryManager) {
     }
 
     /**
-    This function calculate Levenshtein distance using dynamic programming.
-    Levenshtein distance is the minimum number of basic operations like add, delete or substitute
-    for a string A to be string B.
-
-    Formula
-    lev(a, b) = max(a, b) if min(a, b) = 1
-    lev(a, b) = lev(a-1, b-1) if last A == last B
-    lev(a, b) = min(lev(a-1, b), lev(a, b-1), lev(a-1, b-1)) + 1
-
-    In this formula a can refer to string a or length of string b.
+     * Calculate Levenshtein distance using iterative with two matrix rows ways.
+     * Pseudocode of this algorithm is in Wikipedia.
      */
     public static int calculateEditDistance(String a, String b) {
-        // get length of string a and b
-        final var aSize = a.length();
-        final var bSize = b.length();
+        // create two work vectors of integer distances
+        var v0 = new int[b.length() + 1];
+        var v1 = new int[b.length() + 1];
 
-        // create a distance table to store calculation, the size of this
-        // is length a + 1 and length b + 1 because position (0, 0) is used
-        // for empty substring
-        // 100 is the default value, there are no word 100 letters long, so it
-        // will not be a problem
-        final var distance = new int[aSize + 1][bSize + 1];
-        for (var row : distance) {
-            Arrays.fill(row, 100);
+        // initialize v0 (the previous row of distances)
+        // this row is A[0][i]: edit distance for an empty s
+        // the distance is just the number of characters to delete from t
+        for (int i = 0; i <= b.length(); ++i) {
+            v0[i] = i;
         }
 
-        // fill lev(a, b) = max(a, b) if min(a, b) = 1 part, this is actually the top
-        // and left margin of the table
-        distance[0][0] = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            // calculate v1 (current row distances) from the previous row v0
 
-        for (int i = 1; i <= aSize; i++) {
-            distance[i][0] = i;
-        }
+            // first element of v1 is A[i + 1][0]
+            // edit distance is delete (i + 1) chars from s to match empty t
+            v1[0] = i + 1;
 
-        for (int i = 1; i <= bSize; i++) {
-            distance[0][i] = i;
-        }
+            // use formula to fill in the rest of the row
+            for (int j = 0; j < b.length(); ++j) {
+                // calculating costs for A[i + 1][j + 1]
+                final var deletionCost = v0[j + 1] + 1;
+                final var insertionCost = v1[j] + 1;
 
-        // start calculating distance
-        // now table look like something like this
-        // _ _ a b c
-        // _ 0 1 2 3
-        // a 1
-        // b 2
-        // c 3
-        for (int i = 1; i <= aSize; i++) {
-            for (int j = 1; j <= bSize; j++) {
-                // a[1] and table[1] is actually refer to different character
-                if (a.charAt(i - 1) == b.charAt(j - 1)) {
-                    distance[i][j] = distance[i - 1][j - 1];
+                var substitutionCost = 0;
+                if (a.charAt(i) == b.charAt(j)) {
+                    substitutionCost = v0[j];
                 } else {
-                    final var lessA = distance[i - 1][j];
-                    final var lessB = distance[i][j - 1];
-                    final var lessBoth = distance[i - 1][j - 1];
-
-                    distance[i][j] = min(lessA, min(lessB, lessBoth)) + 1;
+                    substitutionCost = v0[j] + 1;
                 }
+
+                v1[j + 1] = min(deletionCost, min(insertionCost, substitutionCost));
             }
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            // since data in v1 is always invalidated, a swap without copy could be more efficient
+            var tmp = v0;
+            v0 = v1;
+            v1 = tmp;
         }
 
-        return distance[aSize][bSize];
+        return v0[b.length()];
     }
 }
